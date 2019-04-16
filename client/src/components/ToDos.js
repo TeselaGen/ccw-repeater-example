6a27b0c34598fbf3398eb8014700ec3faca1141d
Colin Wilson <colin.wilson@teselagen.com>
2019-04-16 15:15:46 -0700

    remove anon function

--- client/src/components/ToDos.js
+++ client/src/components/ToDos.js
@@ -23,6 +23,31 @@ export class ToDos extends Component {
     `;
   }
 
+  LoadToDos({ loading, error, data, refetch, ...rest }) {
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :( something went wrong </p>;
+
+    console.log(rest);
+    console.log(data);
+
+    let todoList = data.todos.results.map(
+      ({ id, user, name, description, status }) => (
+        <div key={id} className={"Todo." + status}>
+          <p>
+            {name}: {description} ({user.firstName})
+          </p>
+        </div>
+      )
+    );
+
+    return (
+      <React.Fragment>
+        {todoList}
+        {<AddTodo refetchTodos={refetch} />}
+      </React.Fragment>
+    );
+  }
+
   render() {
     let page = this.props.page;
     let pageSize = 5;
@@ -32,29 +57,8 @@ export class ToDos extends Component {
     };
     return (
       <Query query={this.myToDoQ} variables={{ pageSize, page, filter }}>
-        {({ loading, error, data, refetch, ...rest }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :( something went wrong </p>;
-
-          console.log(rest);
-          console.log(data);
-
-          let todoList = data.todos.results.map(
-            ({ id, user, name, description, status }) => (
-              <div key={id} className={"Todo." + status}>
-                <p>
-                  {name}: {description} ({user.firstName})
-                </p>
-              </div>
-            )
-          );
-
-          return (
-            <React.Fragment>
-              {todoList}
-              {<AddTodo refetchTodos={refetch} />}
-            </React.Fragment>
-          );
+        {reactObj => {
+          return this.LoadToDos(reactObj);
         }}
       </Query>
     );
