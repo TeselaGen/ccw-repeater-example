6ae28f3825a11c7258586cba6561954f8b895527
Adam Thomas <adam@teselagen.com>
2019-04-17 23:37:49 -0400

    got apollo client using correct url

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,7 +1,12 @@
 import ApolloClient from "apollo-boost";
+import url from "url";
+let currentUrl = url.parse(window.location.href);
+// console.log("currentUrl: ", currentUrl);
+let graphQlUrl = `${currentUrl.protocol}//${currentUrl.host}/graphql`;
+console.log(`GraphQL Endpoint: ${graphQlUrl}`);
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql"
+  uri: graphQlUrl
 });
 
 export default client;
